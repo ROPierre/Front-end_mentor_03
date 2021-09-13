1b4ba48e9539e1d3ba2daf708fc0e30f2fc23df5
rafaelopierre04 <rafaelopierre@gmail.com>
2021-09-13 14:30:37 -0300

    Final commit

--- README.md
+++ README.md
@@ -27,9 +27,6 @@ Users should be able to:
 - View the optimal layout for the component depending on their device's screen size
 - See the social media share links when they click the share icon
 - See how I did the challenge
-### Screenshot
-
-![](./screenshot.jpg)
 
 ### Links
 
@@ -38,7 +35,9 @@ Users should be able to:
 
 ## My process
 
-On Progress
+First of all, I started doing what I wanted to do, build a script of where all elements should be. This doesn't mean that I can't change if the code asks for it. And then started to code the HTML and the CSS (trying to get the main properties from each element) prioritizing mobile screens.
+Secondly, I started the javascript, did the interactions and made adjustments on the elements to respect the layout.
+Finally, I tested on deploy.  
 
 ### Built with
 
@@ -51,19 +50,44 @@ On Progress
 
 ### What I learned
 
-On Progress
+- JS
+* How to position elements getting the X and Y axis from another element 
+```javascript
+const boundBox = parentElement.getBoundingClientRect();
+const x = boundBox.left;
+const y = boundBox.top;
+element.style.left = `${x - 130}px`;
+element.style.top = `${y - 70}px`;
+```
+
+* How to toggle a class on pure JS (vanilla). I used to do the hard way and never thought about ClassList having a toggle method there.
+  * What I used to do:
+  ```javascript
+  if(element.classList.contain("class")){
+    element.classList.remove("class")
+  } else {
+    element.classList.add("class")
+  }
+  ```
+  * Using toggle:
+  ```javascript
+  element.classList.toggle("class");
+  ``` 
 
 ### Continued development
 
-On Progress
+For next challenge I'll be tempting to do something more complete, maybe a landing page or something focus on JS and the landing page. The next step (after the next challenge), I am going to study more about SASS and CSS GRID, then REACT and VUE (I set my due date to 20/12, hope to archive that)
 
 ### Useful resources
 
-On Progress
+- W3Schools
+- MDN Mozilla
+- Alura (review my studies)
+- Stack Overflow
 
 ## Author
 
-- Website - [Add your name here](https://github.com/ROPierre)
+- Github - [@ROPierre](https://github.com/ROPierre)
 - Frontend Mentor - [@ROPierre](https://www.frontendmentor.io/profile/ROPierre)
 
 ## Acknowledgments
--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 const shareButtons = document.getElementsByClassName("share");
+const closeSocialButton = document.getElementsByClassName("social-close");
+let social = document.getElementById("social");
 
-let tooltip = document.getElementById("tooltip");
-
-// Add Event Listener to all elements in shareButtons
+// Add Event Listener to all elements in shareButtons desktop Event
 for (let i = 0; i < shareButtons.length; i++) {
     shareButtons[i].addEventListener("click", function () {
-        // Setting the rectangle from the element
-        const boundBox = this.getBoundingClientRect();
-        const x = boundBox.left;
-        const y = boundBox.top;
-        // Setting the coordinates from the tooltip
-        tooltip.style.left = `${x - 130}px`;
-        tooltip.style.top = `${y - 70}px`;
-        tooltip.className = "visible";
-        tooltip.focus();
-
+        if (window.screen.width > 720) {
+            // Setting the rectangle from the element
+            const boundBox = this.getBoundingClientRect();
+            const x = boundBox.left;
+            const y = boundBox.top;
+            // Setting the coordinates from the tooltip
+            social.style.left = `${x - 130}px`;
+            social.style.top = `${y - 70}px`;
+            social.classList.add("social-desktop");
+            social.classList.add("visible");
+            social.focus();
+        }
+        else {
+            social.classList.toggle("visible");
+            shareButtons[i].parentNode.classList.toggle("hide");
+            social.focus();
+        }
         shareButtons[i].classList.add("share-active");
     });
-    
 }
 
-tooltip.addEventListener("focusout", function () {
-    console.log('xuxa')
-    if (tooltip.classList == "visible") {
-        tooltip.classList.remove("visible");
-        tooltip.className = "hide";
-    } else {
-        console.log(0)
+for (let i = 0; i < closeSocialButton.length; i++) {
+    closeSocialButton[i].addEventListener("click", function () {
+        social.classList.toggle("visible");
+        shareButtons[i].parentNode.classList.toggle("hide");
+        social.focus();
+        closeSocialButton[i].classList.add("share-active");
+    });
+}
+
+social.addEventListener("focusout", function () {
+    if (social.classList.contains("social-desktop")) {
+        social.classList.remove('visible');
+        social.classList.add('hide');
     }
 })
